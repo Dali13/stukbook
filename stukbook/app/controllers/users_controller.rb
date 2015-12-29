585882c0a177f7831956b903596a5a78d9ad82b8
Dali <daly_toulousain@hotmail.fr>
2015-12-29 12:47:45 +0100

    Added users index

--- stukbook/app/controllers/users_controller.rb
+++ stukbook/app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
 	before_action :set_user, only:[:show]
-  def show
-  end
 
-  private
-  	def set_user
-  		@user = User.find_by(:username params[:id])
+	def index
+		@users = User.all
+	end
+
+  	def show
   	end
+
+	  private
+	  	def set_user
+	  		@user = User.find_by(username: params[:id])
+	  	end
 end
