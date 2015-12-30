cdf30d46ede7a26a6c66e82862416f12d1fed7fc
Dali <daly_toulousain@hotmail.fr>
2015-12-30 14:36:00 +0100

    Added user detection

--- stukbook/app/helpers/users_helper.rb
+++ stukbook/app/helpers/users_helper.rb
@@ -1,15 +1,14 @@
 module UsersHelper
 
 	def action_buttons(user)
-		case current_user.friendship_status(user) 
-			when "friends"
+		case current_user.friendship_status(user) when "friends"
 			link_to "Cancel friendship", friendship_path(current_user.friendship_relation(user)), method: :delete
 			when "pending"
 			link_to "Cancel request", friendship_path(current_user.friendship_relation(user)), method: :delete
 			when "requested"
 			link_to "Accept request", accept_friendship_path(current_user.friendship_relation(user)), method: :put
-			when "not_friends"
-		    link_to "Add as friend", friendships_path(user_id: user.id), method: :post
+			when "not friends"
+		  link_to "Add as friend", friendships_path(user_id: user.id), method: :post
 		end
 	end
 
